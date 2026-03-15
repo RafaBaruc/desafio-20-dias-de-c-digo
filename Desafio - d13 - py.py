6e6ddfe91bbefc02f93f757999d0e4b1dbef8147
Rafael Baruc <141531326+RafaBaruc@users.noreply.github.com>
2026-03-15 11:13:15 -0300

    Resolução desafio dia 13

---
+++ Desafio - d13 - py.py
@@ -0,0 +1,12 @@
+a, b, c = map(float, input().split())
+tri = a * c / 2
+cir = 3.14159 * c * c
+tra = (a + b) * c / 2
+qua = b * b
+ret = a * b
+
+print(f"TRIANGULO: {tri:.3f}")
+print(f"CIRCULO: {cir:.3f}")
+print(f"TRAPEZIO: {tra:.3f}")
+print(f"QUADRADO: {qua:.3f}")
+print(f"RETANGULO: {ret:.3f}")
